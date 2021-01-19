eeed436089e1f46381bef2709814a606ae9b7dfb
Anderson Lemos <anderson.lemos@amedigital.com>
2021-01-19 19:14:01 -0300

    refactoring list

--- fizzbuzz/lib/fizzbuzz.ex
+++ fizzbuzz/lib/fizzbuzz.ex
@@ -1,9 +1,14 @@
 defmodule Fizzbuzz do
   def build(file_name) do
-   file_name
-  |> File.read()
-  |> handle_file_read()
+    file_name
+    |> File.read()
+    |> handle_file_read()
   end
-  def handle_file_read({:ok, result}), do: result
+
+  def handle_file_read({:ok, result}) do
+    result
+    |> String.split(",")
+    |> Enum.map(&String.to_integer/1)
+  end
   def handle_file_read({:error, reason}), do: reason
 end
