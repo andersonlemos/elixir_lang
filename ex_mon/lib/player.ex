f6a75d4fe5321d652e09422ffe6096b2cb8427c1
Anderson Lemos <anderson.lemos@amedigital.com>
2021-01-19 20:14:25 -0300

    refactoring struct

--- ex_mon/lib/player.ex
+++ ex_mon/lib/player.ex
@@ -1,15 +1,16 @@
 defmodule ExMon.Player do
-  @enforce_keys [:life, :name, :move_rnd, :move_avg, :move_heal]
+  @required_keys [:life, :name, :move_rnd, :move_avg, :move_heal]
+  @max_life 100
 
-  defstruct [:life, :name, :move_rnd, :move_avg, :move_heal]
-
+  @enforce_keys @required_keys
+  defstruct @required_keys
   def build(name, move_rnd, move_avg, move_heal) do
      %ExMon.Player{
        name: name,
        move_rnd: move_rnd,
        move_avg: move_avg,
        move_heal: move_heal,
-       life: 100
+       life: @max_life
      }
   end
 end
